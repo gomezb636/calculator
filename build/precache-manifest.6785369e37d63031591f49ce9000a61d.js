92a9096ed0e1f2bea5ea99e92a544438d4e37bf8
gomezb636 <gomezb636@csu.fullerton.edu>
2021-12-02 23:17:18 -0800

    Fixed bug

---
+++ build/precache-manifest.6785369e37d63031591f49ce9000a61d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ca463033d1dfb87abd8",
+    "url": "/static/css/main.64a7d227.chunk.css"
+  },
+  {
+    "revision": "4ca463033d1dfb87abd8",
+    "url": "/static/js/main.4ca46303.chunk.js"
+  },
+  {
+    "revision": "22c1a6d252f100d076b2",
+    "url": "/static/js/1.22c1a6d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "222740767d528029de14f7915a76da16",
+    "url": "/index.html"
+  }
+];
